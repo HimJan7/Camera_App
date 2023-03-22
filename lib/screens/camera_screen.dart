14184caae692beb263f8df9dd22f5598fa160494
Himanshu Jangra <himjan0@gmail.com>
2023-03-22 18:01:56 +0530

    [zoomLevel]

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -22,6 +22,19 @@ class _CameraScreenState extends State<CameraScreen>
   final resolutionPresets = ResolutionPreset.values;
   ResolutionPreset currentResolutionPreset = ResolutionPreset.high;
 
+  bool _isCameraPermissionGranted = false;
+  bool _isVideoCameraSelected = false;
+  bool _isRecordingInProgress = false;
+  double _minAvailableExposureOffset = 0.0;
+  double _maxAvailableExposureOffset = 0.0;
+  double _minAvailableZoom = 1.0;
+  double _maxAvailableZoom = 1.0;
+
+  // Current values
+  double _currentZoomLevel = 1.0;
+  double _currentExposureOffset = 0.0;
+  FlashMode? _currentFlashMode;
+
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     final CameraController? cameraController = controller;
@@ -37,6 +50,11 @@ class _CameraScreenState extends State<CameraScreen>
     }
   }
 
+  void resetCameraValues() async {
+    _currentZoomLevel = 1.0;
+    _currentExposureOffset = 0.0;
+  }
+
   void onNewCameraSelected(CameraDescription cameraDescription) async {
     final previousCameraController = controller;
 
@@ -45,7 +63,10 @@ class _CameraScreenState extends State<CameraScreen>
       currentResolutionPreset,
       imageFormatGroup: ImageFormatGroup.jpeg,
     );
+
     await previousCameraController?.dispose();
+
+    resetCameraValues();
 
     if (mounted) {
       setState(() {
@@ -53,12 +74,29 @@ class _CameraScreenState extends State<CameraScreen>
       });
     }
 
+    // Update UI if controller updated
     cameraController.addListener(() {
       if (mounted) setState(() {});
     });
 
     try {
       await cameraController.initialize();
+      await Future.wait([
+        cameraController
+            .getMinExposureOffset()
+            .then((value) => _minAvailableExposureOffset = value),
+        cameraController
+            .getMaxExposureOffset()
+            .then((value) => _maxAvailableExposureOffset = value),
+        cameraController
+            .getMaxZoomLevel()
+            .then((value) => _maxAvailableZoom = value),
+        cameraController
+            .getMinZoomLevel()
+            .then((value) => _minAvailableZoom = value),
+      ]);
+
+      _currentFlashMode = controller!.value.flashMode;
     } on CameraException catch (e) {
       print('Error initializing camera: $e');
     }
@@ -87,35 +125,87 @@ class _CameraScreenState extends State<CameraScreen>
     return SafeArea(
       child: Scaffold(
         body: _isCameraInitialized
-            ? Column(
+            ? Stack(
                 children: [
                   AspectRatio(
                     aspectRatio: 1 / controller!.value.aspectRatio,
                     child: controller!.buildPreview(),
                   ),
-                  ElevatedButton(
-                      onPressed: () {
+                  Row(
+                    children: [
+                      Slider(
+                        value: _currentZoomLevel,
+                        min: _minAvailableZoom,
+                        max: _maxAvailableZoom,
+                        activeColor: Colors.white,
+                        inactiveColor: Colors.white30,
+                        onChanged: (value) async {
+                          setState(() {
+                            _currentZoomLevel = value;
+                          });
+                          await controller!.setZoomLevel(value);
+                        },
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black87,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            _currentZoomLevel.toStringAsFixed(1) + 'x',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: DropdownButton<ResolutionPreset>(
+                      underline: Container(),
+                      value: currentResolutionPreset,
+                      items: [
+                        for (ResolutionPreset preset in resolutionPresets)
+                          DropdownMenuItem(
+                            child: Text(
+                              preset.toString().split('.')[1].toUpperCase(),
+                              style: TextStyle(color: Colors.black),
+                            ),
+                            value: preset,
+                          )
+                      ],
+                      onChanged: (value) {
                         setState(() {
+                          currentResolutionPreset = value!;
                           _isCameraInitialized = false;
                         });
-                        onNewCameraSelected(
-                          cameras[_isRearCameraSelected ? 0 : 1],
-                        );
-                        setState(() {
-                          _isRearCameraSelected = !_isRearCameraSelected;
-                        });
-
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: const Text(
-                              'Switch Camera',
-                              textAlign: TextAlign.center,
-                            ),
-                            duration: const Duration(seconds: 1),
-                          ),
-                        );
+                        onNewCameraSelected(controller!.description);
                       },
-                      child: Text('SB')),
+                      hint: Text(
+                        "Select item",
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomLeft,
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                    child: IconButton(
+                        iconSize: 50,
+                        onPressed: () {
+                          setState(() {
+                            _isCameraInitialized = false;
+                          });
+                          onNewCameraSelected(
+                            cameras[_isRearCameraSelected ? 0 : 1],
+                          );
+                          setState(() {
+                            _isRearCameraSelected = !_isRearCameraSelected;
+                          });
+                        },
+                        icon: Icon(Icons.cameraswitch_rounded)),
+                  )
                 ],
               )
             : Container(),
